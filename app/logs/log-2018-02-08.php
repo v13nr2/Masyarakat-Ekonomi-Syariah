2df72ddf9484560b060f8a685b62ab770756e290
Nanang Rustianto <nanang.programmer@gmail.com>
2018-02-08 14:34:35 +0800

    Database 2018 February

---
+++ app/logs/log-2018-02-08.php
@@ -0,0 +1,59 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2018-02-08 07:03:47 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:03:47 --> No URI present. Default controller set.
+DEBUG - 2018-02-08 07:03:47 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:03:48 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:03:48 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:03:49 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:03:49 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:03:49 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:03:49 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:03:49 --> Cizacl class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:03:50 --> Total execution time: 0.2764
+DEBUG - 2018-02-08 07:03:50 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:03:50 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:03:50 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:03:50 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:03:50 --> Total execution time: 0.1395
+DEBUG - 2018-02-08 07:03:54 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:03:54 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:03:54 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:03:54 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:03:54 --> Cizacl class already loaded. Second attempt ignored.
+ERROR - 2018-02-08 07:03:54 --> Query error: Unknown column 'awal' in 'field list' - Invalid query: SELECT `awal`, `akhir`
+FROM `trs_tutup_buku_bulanan`
+WHERE `status` = 'A'
+DEBUG - 2018-02-08 07:03:57 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:03:57 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:03:57 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:03:57 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:03:57 --> Cizacl class already loaded. Second attempt ignored.
+ERROR - 2018-02-08 07:03:57 --> Query error: Unknown column 'awal' in 'field list' - Invalid query: SELECT `awal`, `akhir`
+FROM `trs_tutup_buku_bulanan`
+WHERE `status` = 'A'
+DEBUG - 2018-02-08 07:05:00 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:05:00 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:05:00 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:05:00 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:05:00 --> Cizacl class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:05:03 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:05:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:05:03 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:05:03 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:05:04 --> Total execution time: 0.5269
+DEBUG - 2018-02-08 07:05:04 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:05:04 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:05:04 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:05:04 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:05:05 --> Total execution time: 0.2039
+DEBUG - 2018-02-08 07:05:12 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:05:12 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:05:12 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:05:12 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:05:12 --> Total execution time: 0.4778
+DEBUG - 2018-02-08 07:05:13 --> UTF-8 Support Enabled
+DEBUG - 2018-02-08 07:05:13 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2018-02-08 07:05:13 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2018-02-08 07:05:13 --> Config file loaded: D:\XAMPP\htdocs\mes2\app\config/cizacl.php
+DEBUG - 2018-02-08 07:05:13 --> Total execution time: 0.1055
